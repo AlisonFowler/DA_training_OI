df169225c0eee66430d638411ac0b4dcbd420a2b
AlisonFowler <64210810+AlisonFowler@users.noreply.github.com>
2023-03-28 11:44:22 +0100

    Update 1_optimal_interpolation.py
    completed conclusions

--- 1_optimal_interpolation.py
+++ 1_optimal_interpolation.py
@@ -181,7 +181,9 @@ st.markdown('We performed simple data assimilation experiments, changing the obs
             'observation uncertainty draws the analysis closer to observations, whereas '
             'large observation uncertainty means that the analysis is more similar to the '
             'background. A small background error correlation length scale indicates that changes in one '
-            'variable affect only the variables that are close to it.')
+            'variable affect only the variables that are close to it.'
+            'A large observation error correlation length scale allows observations'
+            'to provide more information on the spatial gradients of the state variable.')
 
 st.subheader("Acknowledgements")
 st.markdown("This code has been created by authors from the "
